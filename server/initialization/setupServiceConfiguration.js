ad6c9951106c19247b981512a6316ea30614fd7f
Veszelovszki Dávid <veszelovszki@gmail.com>
2015-12-02 20:58:07 +0100

    Added ServiceConfiguration package.

---
+++ server/initialization/setupServiceConfiguration.js
@@ -0,0 +1,10 @@
+// First, remove configuration entry in case service is already configured
+ServiceConfiguration.configurations.remove({
+  service: "google"
+});
+ServiceConfiguration.configurations.insert({
+  service: "google",
+  clientId: "335972580959-rh6bp65u4e3gfj9eng1s6burtundhlos.apps.googleusercontent.com",
+  loginStyle: "popup",
+  secret: "G1hCq4SWVzknc0BazaIJRwz7"
+});
