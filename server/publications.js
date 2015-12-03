58353f9f5ae7c423b980b930b016de8c32dd06cf
Veszelovszki Dávid <veszelovszki@gmail.com>
2015-12-03 11:51:08 +0100

    Added client-side user data access.

--- server/publications.js
+++ server/publications.js
@@ -6,3 +6,12 @@ Meteor.publish('reviews', ((filter)=> {
 Meteor.publish('review', ((id)=> {
   return Reviews.findOne({_id: id});
 }));
+
+Meteor.publish('user', function () {
+  if (this.userId) {
+    return Meteor.users.find({_id: this.userId},
+      {fields: {'profile.name': 1, 'things': 1}});
+  } else {
+    this.ready();
+  }
+});
